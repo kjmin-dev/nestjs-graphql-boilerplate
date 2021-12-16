a6253b44f0ba28c32de31c46685b106d4c467841
kjmin <kjmin@covision.co.kr>
2021-12-16 17:49:19 +0900

    Add Date custom scalar

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { PrismaService } from './services/prisma.service';
+import { DateScalar } from './graphql/scalars/DateScalar';
 import { join } from 'path';
 
 @Module({
@@ -11,6 +12,6 @@ import { join } from 'path';
     }),
   ],
   controllers: [],
-  providers: [PrismaService],
+  providers: [PrismaService, DateScalar],
 })
 export class AppModule {}
