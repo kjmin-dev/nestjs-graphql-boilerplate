d2564b90b147caa1c2e8d66c78b66203f16510b5
kjmin <kjmin@covision.co.kr>
2021-12-17 10:26:06 +0900

    Replace playground > landing page

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { PrismaService } from './services/prisma.service';
 import { TodoListResolver } from './graphql/todo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
+      playground: false,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       buildSchemaOptions: { dateScalarMode: 'timestamp' },
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
   ],
   controllers: [],
